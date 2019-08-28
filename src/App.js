94ebdd580945848630e7faa827fbd4afb3595334
Nicholas Wease <skynet60@outlook.com>
2019-08-28 09:47:54 +0200

    Added Header component and links to home as well as some styling.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 import Shop from './pages/shop/Shop';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+        <Header />
+
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={Shop} />
--- src/components/items/Items.jsx
+++ src/components/items/Items.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import './styles/items.scss';
 
-const Items = ({id, name, price, imageUrl}) => {
-    return (
-        <div className='item'>
-            <div
-                className='image'
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }}>
+const Items = ({id, name, price, imageUrl}) => (
+    <div className='item'>
+        <div
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
 
-                <div className='footer'>
-                    <span className='name'>
-                        {name}
-                    </span>
+            <div className='footer'>
+                <span className='name'>
+                    {name}
+                </span>
 
-                    <span className='price'>
-                        {price}
-                    </span>
-                </div>
+                <span className='price'>
+                    {price}
+                </span>
             </div>
         </div>
     );
-};
 
 export default Items;
