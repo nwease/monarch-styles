4909f98b845d14dbe9830fefc6cdd9b7b732c89c
Nicholas Wease <skynet60@outlook.com>
2019-09-08 19:36:08 +0200

    Updated more stripe integration along with updated favicon and title.

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -51,6 +51,12 @@ const Checkout = ({cartItems, total}) => {
                 </span>
             </div>
 
+            <div className='test-warning'>
+                *Please use the following test credit card for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+            </div>
+
             <Stripe price={total} />
 
         </div>
